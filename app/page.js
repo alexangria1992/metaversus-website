d4f6c7af7361868df27b37aba98bcfdb510b6c5f
alexangria1992 <alex040892@gmail.com>
2023-09-23 13:34:23 +1200

    World and Insights

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-    <div className="relative ">
+    <div className="relative">
       <About />
       <div className="gradient-03 z-0" />
       <Explore />
@@ -24,15 +24,12 @@ const Page = () => (
       <div className="gradient-04 z-0" />
       <WhatsNew />
     </div>
-
     <World />
     <div className="relative">
       <Insights />
       <div className="gradient-04 z-0" />
-
       <Feedback />
     </div>
-
     <Footer />
   </div>
 );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+
 import styles from "../styles";
 import { navVariants } from "../utils/motion";
 
@@ -11,22 +12,19 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div
-      className="absolute w-[50%] inset-0  
-    gradient-01"
-    />
+    <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
         src="/search.svg"
-        alt=""
+        alt="search"
         className="w-[24px] h-[24px] object-contain"
       />
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
         METAVERSUS
       </h2>
       <img
         src="/menu.svg"
-        alt=""
+        alt="menu"
         className="w-[24px] h-[24px] object-contain"
       />
     </div>
